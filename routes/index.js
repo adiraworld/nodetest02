52552d9f89a85341c411e3f136edefaabbceaaab
Richard Kang <adiraworld@gmail.com>
2014-10-26 04:18:49 +0900

    file upload 1027

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	
+	var data = {
+	    title: "express:",
+	    table: [
+	        {filename: "Ted", type: "jpg", URL: "goo.gl/image01.jpg"},
+	        {filename: "Bob", type: "jpg", URL: "goo.gl/image02.jpg"}
+	    ]
+	};
+	
+	res.render('index', data);
 });
 
 module.exports = router;
